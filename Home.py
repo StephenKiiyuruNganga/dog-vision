5f91d482a42b2e5be9f4d528c9ad21c48df92c90
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2023-03-27 08:54:21 +0300

    refactored logic thanks to ChatGPT

--- Home.py
+++ Home.py
@@ -3,11 +3,7 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
-
-
-@st.cache_data
-def get_labels():
-    return pd.read_csv("train/labels.csv")
+from PIL import Image
 
 
 @st.cache_resource
@@ -20,24 +16,8 @@ def load_model():
 
 
 @st.cache_data
-def process_img(_img):
-    image = tf.image.decode_jpeg(_img, channels=3)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.image.resize(image, size=[224, 224])
-    return image
-
-
-@st.cache_data
-def create_batches(_img):
-    data_set = tf.data.Dataset.from_tensor_slices(_img)
-    data_set = data_set.map(process_img)
-    batches = data_set.batch(32)
-    return batches
-
-
-@st.cache_data
-def get_name(prediction):
-    return unique_breeds[np.argmax(prediction)]
+def get_labels():
+    return pd.read_csv("train/labels.csv")
 
 
 "# Dog Vision 🐶"
@@ -47,36 +27,34 @@ df_labels = get_labels()
 labels = np.array(df_labels["breed"])
 unique_breeds = np.unique(labels)
 
-file = st.file_uploader(
+uploaded_file = st.file_uploader(
     label="Upload an image of a dog",
     type=["png", "jpg"],
 )
 
-if file is not None:
-    user_img = file.read()
-    step_1 = st.text(f"Processing '{file.name}'... 🔁")
-    img = None
-    img = process_img(user_img)
+if uploaded_file is not None:
+    # Read the image using PIL
+    img = Image.open(uploaded_file)
+    step_1 = st.text(f"Processing '{uploaded_file.name}'... 🔁")
 
-    if img is not None:
-        step_1.text(f"Processing '{file.name}'... ✅")
-        # st.write(img.shape)
-        step_2 = st.text(f"Batching '{file.name}'... 📦")
-        batches = None
-        data_set = tf.data.Dataset.from_tensor_slices(img)
-        data_set = data_set.map(process_img)
-        batches = data_set.batch(32)
+    # Resize the image to the required size
+    img = img.resize((224, 224))
 
-        if batches is not None:
-            step_2.text(f"Batching '{file.name}'... ✅")
-            # st.write(batches.element_spec)
-            step_3 = st.text(f"Analyzing canine features of '{file.name}'... 🤔")
-            pred = None
-            pred = model.predict(batches, verbose=1)
+    # Convert the image to a numpy array
+    img_array = np.array(img)
 
-            if pred is not None:
-                step_3.text(f"Analyzing canine features of '{file.name}'... ✅")
-                label = get_name(pred)
+    # Normalize the image pixel values to be in the range of [0, 1]
+    img_array = img_array / 255.0
 
-                if label:
-                    st.image(img.numpy(), caption=label)
+    # Add a batch dimension to the array
+    img_array = np.expand_dims(img_array, axis=0)
+    step_1.text(f"Processing '{uploaded_file.name}'... ✅")
+
+    # Use the loaded model to make predictions on the preprocessed image
+    step_2 = st.text(f"Analyzing '{uploaded_file.name}'... 🤔")
+    prediction = model.predict(img_array, verbose=1)
+    label = unique_breeds[np.argmax(prediction)]
+    step_2.text(f"Analyzing '{uploaded_file.name}'... ✅\n")
+
+    # Display the converted image and label
+    st.image(img_array, caption=label)
